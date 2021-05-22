8389f30ae953fcc0033f76cbbafe0f501a235b47
Surendra Bajracharya <bajracharya.surendra@gmail.com>
2021-05-22 21:10:52 +0100

    useful tips for static mocks using mockto

---
+++ mockito/src/main/java/com/surendra/usefultips/UsefulTipsApplication.java
@@ -0,0 +1,13 @@
+package com.surendra.usefultips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefulTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsefulTipsApplication.class, args);
+	}
+
+}
---
+++ mockito/src/test/java/com/surendra/usefultips/UsefulTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surendra.usefultips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
