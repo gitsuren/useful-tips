290841c06683eca08299ee695198901e464b5f91
Surendra Bajracharya <bajracharya.surendra@gmail.com>
2021-05-22 22:28:47 +0100

    Added the BDD cucumber example helloworld from baeldung as well

---
+++ BDD/hello-cucumber/src/test/java/com/surendra/hellocucumber/HelloCucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surendra.hellocucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCucumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
