39e37729567eab6d285da0418eaebb81f761a8ca
Surendra Bajracharya <bajracharya.surendra@gmail.com>
2021-05-27 11:53:36 +0100

    Added another tip with integration tests for kafka producers and consumers with embedded kafka as well as using testContainers dockers, it works!

---
+++ spring-kafka-mock/src/main/java/com/surendra/springkafkamock/SpringKafkaMockApplication.java
@@ -0,0 +1,13 @@
+package com.surendra.springkafkamock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaMockApplication.class, args);
+	}
+
+}
---
+++ spring-kafka-mock/src/test/java/com/surendra/springkafkamock/SpringKafkaMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surendra.springkafkamock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
